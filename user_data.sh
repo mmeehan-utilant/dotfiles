9c986a3aa7541dc094ffa4dca8ba7dac43930223
Matt Meehan <mmeehan@utilant.com>
2021-06-11 11:43:27 -0500

    reverting to erogers setup

--- user_data.sh
+++ user_data.sh
@@ -17,22 +17,39 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get -y update
 
 # Install zsh
-printf "\nInstalling zsh...\n"
 apt-get -y install zsh
 cp -p /etc/pam.d/chsh /etc/pam.d/chsh.backup
 sed -ri "s|auth( )+required( )+pam_shells.so|auth sufficient pam_shells.so|" /etc/pam.d/chsh
 
-
-apt-get -y install tmux
+# Install pyenv prerequisites
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+     libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+     xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
 
 # Install mount_volume script
 cp -p mount_volume.sh /usr/local/bin/mount_volume
 chown ${NONROOT_USER}: /usr/local/bin/mount_volume
 
 # Install add-jupyter-kernel script
-printf "\nInstalling add-jupyter-kernel...\n"
 cp -p add-jupyter-kernel /usr/local/bin/add-jupyter-kernel
 chown ${NONROOT_USER}: /usr/local/bin/add-jupyter-kernel
+
+# Install emacs
+apt-get -y install emacs
+
+# Install python3 & pip
+apt-get install -y \
+	python3 \
+	python3-pip
+
+# Install other tools
+apt-get -y install \
+	curl \
+	docker \
+	htop \
+	tmux \
+	unzip
+
 
 unset DEBIAN_FRONTEND
 
